3efc6f202ad27f9d6fd6a2d8bb892b2b991c77fb
Psiyllo <paulocesaralcabral@gmail.com>
2025-11-17 09:07:27 -0300

    test: cenarios de atualizar carro

--- src/test/java/io/github/Psiyllo/locadora/service/CarroServiceTest.java
+++ src/test/java/io/github/Psiyllo/locadora/service/CarroServiceTest.java
@@ -1,7 +1,9 @@
 package io.github.Psiyllo.locadora.service;
 
 import io.github.Psiyllo.locadora.entity.CarroEntity;
+import io.github.Psiyllo.locadora.model.Exceptions.EntityNotFoundException;
 import io.github.Psiyllo.locadora.repository.CarroRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -49,6 +51,39 @@ class CarroServiceTest {
 
         assertThat(erro).isInstanceOf(IllegalArgumentException.class);
 
+        Mockito.verify(repository, Mockito.never()).save(Mockito.any());
+
+    }
+
+    @Test
+    void deveAtualizarUmCarro(){
+
+        var carroExistente = new CarroEntity("Palio", 10, 2013);
+        Mockito.when(repository.findById(1L)).thenReturn(Optional.of(carroExistente));
+
+        var carroAtualizado = new CarroEntity("Passat", 20, 2013);
+        carroAtualizado.setId(1L);
+        Mockito.when(repository.save(Mockito.any())).thenReturn(carroAtualizado);
+
+        Long id = 1L;
+        var carro = new CarroEntity("Sedan", 0, 2026);
+
+        var resultado = service.atualizar(id, carro);
+
+        assertEquals(resultado.getModelo(),"Passat");
+        Mockito.verify(repository, Mockito.times(1)).save(Mockito.any());
+    }
+
+    @Test
+    void deveDarErroAoTentarAtualizarCarroInexistente(){
+        Long id = 1L;
+
+        var carro = new CarroEntity("Palio", 10, 2013);
+        Mockito.when(repository.findById(Mockito.any())).thenReturn(Optional.empty());
+
+        var erro = catchThrowable(() -> service.atualizar(id, carro));
+
+        assertThat(erro).isInstanceOf(EntityNotFoundException.class);
         Mockito.verify(repository, Mockito.never()).save(Mockito.any());
 
     }
